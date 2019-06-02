20d2a26495bb5cd02564c4ab9e4ef7ca02aa5aa9
Chris Newton <chris.newton@revium.com.au>
2019-06-03 08:53:24 +1000

    inital commit of workflow

---
+++ git2.js
@@ -0,0 +1,59 @@
+import { createGitgraph, TemplateName, templateExtend } from "@gitgraph/js";
+// Get the graph container HTML element.
+const graphContainer = document.getElementById("regularFlowPmChange");
+
+// Instantiate the graph.
+const Revium = templateExtend(TemplateName.Metro, {
+  colors: ["#0C0A3E", "#7B1E7A", "#B33F62", "#F9564F", "#F3C677"],
+  branch: {
+    lineWidth: 4,
+    spacing: 50,
+    label: {
+      font: "normal 10pt Arial",
+      displayAuthor: false
+    }
+  },
+  commit: {
+    spacing: 60,
+    dot: {
+      size: 16,
+      strokeColor: "#000000",
+      strokeWidth: 2
+    },
+    message: {
+      displayAuthor: false,
+      font: "normal 10pt Arial"
+    }
+  }
+});
+const gitgraph = createGitgraph(graphContainer, {
+  template: Revium
+});
+
+// Simulate git commands with Gitgraph API.
+const master = gitgraph.branch("master");
+master.commit("Initial commit");
+
+const staging = gitgraph.branch("staging");
+staging.commit("Initial commit");
+
+const dev = gitgraph.branch("dev");
+dev.commit("Initial commit");
+
+const feature1 = gitgraph.branch("feature/new-navigation-menu");
+feature1
+  .commit("created navigation")
+  .commit("updated navigation js so that it works in IE11")
+  .commit("added hover state color for desktop");
+
+dev.merge(feature1);
+
+feature1
+  .commit("PM noticed bug")
+  .commit("fixed an issue on mobile where button could not be clicked");
+
+dev.merge(feature1);
+
+staging.merge(dev);
+
+master.merge(staging);
---
+++ git3.js
@@ -0,0 +1,55 @@
+import { createGitgraph, TemplateName, templateExtend } from "@gitgraph/js";
+// Get the graph container HTML element.
+const graphContainer = document.getElementById("hotfixChange");
+
+// Instantiate the graph.
+const Revium = templateExtend(TemplateName.Metro, {
+  colors: ["#0C0A3E", "#7B1E7A", "#B33F62", "#F9564F", "#F3C677"],
+  branch: {
+    lineWidth: 4,
+    spacing: 50,
+    label: {
+      font: "normal 10pt Arial",
+      displayAuthor: false
+    }
+  },
+  commit: {
+    spacing: 60,
+    dot: {
+      size: 16,
+      strokeColor: "#000000",
+      strokeWidth: 2
+    },
+    message: {
+      displayAuthor: false,
+      font: "normal 10pt Arial"
+    }
+  }
+});
+const gitgraph = createGitgraph(graphContainer, {
+  template: Revium
+});
+
+// Simulate git commands with Gitgraph API.
+const master = gitgraph.branch("master");
+master.commit("Initial commit");
+
+const staging = gitgraph.branch("staging");
+staging.commit("Initial commit");
+
+const dev = gitgraph.branch("dev");
+dev.commit("Initial commit");
+
+const feature1 = gitgraph.branch("feature/new-navigation-menu");
+
+feature1
+  .commit("created navigation")
+  .commit("updated navigation js so that it works in IE11");
+
+const hotfix = master.branch("hotfix/ie-11-bug");
+
+hotfix.commit("fixed grid compatibility issue in ie 11");
+
+master.merge(hotfix);
+
+feature1.commit("added hover state color for desktop");
